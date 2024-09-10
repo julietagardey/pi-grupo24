7207c1b1f1d89806ce7f5339266b2a414e110820
moranoguer <nmora@udesa.edu.ar>
2024-09-10 19:59:19 -0300

    mas header

--- pi-grupo24/src/components/Header/Header.js
+++ pi-grupo24/src/components/Header/Header.js
@@ -1,11 +1,32 @@
-import Navbar from '../Navbar/Navbar'
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css"
+
 const Header = () => {
     return (
-        <>
-            <Navbar></Navbar>
-        </>
+        <header>
+            <section className="headerIzquierda">
+                <img className="logo" src="./img/logo-img.png" alt="logo" />
+
+                <Link to="/" className="home">
+                    <h2 className="home">Inicio</h2>
+                </Link>
+            </section>
+            <nav className="headerDerecha">
+                <article className="linksHeader">
+                    <Link to="/Favorites" className="linkfavoritos">
+                        Favoritos
+                    </Link>
+                    <Link to="/NowPlayingVista" className="linkfavoritos">
+                        En Cartel
+                    </Link>
+                    <Link to="/TopRatedVista" className="linkfavoritos">
+                        Más Populares
+                    </Link>
+                </article>
+            </nav>
+        </header>
     )
-} 
+}
 
 export default Header
